5467e5d2e568817b41562b87f0b769f87f0bf001
juancarlos.camara.checa.next <juancarlos.camara.checa.next@bbva.com>
2020-11-17 16:33:25 +0100

    Included some files in gitignore file

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,12 @@
 from flask import Flask
 application = Flask(__name__)
 
+
 @application.route("/")
+@application.route("/index")
 def hello():
     return "Hello World!"
+
 
 if __name__ == "__main__":
     application.run()
